0d8d251e1bf1127bb48432f6b9c92ce77f0e987a
Colin McKay <cgmckay@wisc.edu>
2013-05-16 09:52:33 -0500

    project finished
    there is still some sort of bug.  According to the TA, the plots are too
    flat.

--- project/plotSpeechInfo.py
+++ project/plotSpeechInfo.py
@@ -1,11 +1,25 @@
 import numpy
 import matplotlib
-import matplotlib.pyplot as plot
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import sys
 import glob
 import re
 import operator
+def parseFileName(filename):
+    fileNumber = re.findall("\d+",filename)
+    fileMostLeast = re.findall("Most|Least",filename)
+    if(fileNumber):
+        if(fileMostLeast):
+            return str(fileNumber[0]+" "+fileMostLeast[0]+" Recent")
+        else:
+            print "error in parsing Most and Least speech"
+            return "NO NAME"
+    else:
+        print "error when parsing number of speeches"
+        return "NO NAME"
+    #return re.sub("[:num]"
 def parseFile(filename):
     f = open(filename)
     fileDictionary = {}
@@ -13,52 +27,57 @@ def parseFile(filename):
         fileDictionary[str(re.sub("../speeches/",'',l.split()[0]))] = float(l.split()[1])
     return fileDictionary
 
-def createPlot(dictionary,pdf):
+def createPlot(dictionary,pdf,plotName = "No Name"):
+    plotName = parseFileName(plotName)
     listOfDictionary = sorted(dictionary.iteritems(), key = operator.itemgetter(0))
     yearsList = []
     numbersList = []
     for e in listOfDictionary:
         yearsList.append(float(re.sub(".txt","",re.sub("_","",e[0])))/100000000.0)
         numbersList.append(e[1])
-    plot.plot(yearsList,numbersList)
-    plot.ylabel('compression ratio')
-    plot.xlabel('year')
+    plot = plt.subplot(111)
+    plot.plot(yearsList,numbersList,label = plotName)
+    #plot.ylabel('compression ratio')
+    #plot.xlabel('year')
+    plot.legend(loc='upper center',bbox_to_anchor=(0.5,1.05),ncol=3,fancybox=True,shadow=True)
     pdf.savefig()
     #pdf.savefig(fig)
 def pdfMostRecentSpeeches():
     listOfInformationFiles = glob.glob("./*MostRecentSpeech.txt")
-    fig = plot.figure(figsize = (8.27, 8.27),dpi = 100)
+    fig = plt.figure(figsize = (8.27, 8.27),dpi = 100)
+    
     for f in listOfInformationFiles:
         pdf = PdfPages('mostRecentSpeeches.pdf')
-        createPlot(parseFile(f),pdf)
+        createPlot(parseFile(f),pdf,f)
         pdf.close()
 def pdfLeastRecentSpeeches():
-    fig = plot.figure(figsize = (8.27, 8.27),dpi = 100)
+    fig = plt.figure(figsize = (8.27, 8.27),dpi = 100)
     listOfInformationFiles = glob.glob("./*LeastRecentSpeech.txt")
     for f in listOfInformationFiles:
+        parseFileName(f)
         pdf = PdfPages('leastRecentSpeeches.pdf')
-        createPlot(parseFile(f),pdf)
+        createPlot(parseFile(f),pdf,f)
         pdf.close()
 def pdf1MostRecentSpeeches():
-    fig = plot.figure(figsize = (8.27, 8.27),dpi = 100)
+    fig = plt.figure(figsize = (8.27, 8.27),dpi = 100)
     listOfInformationFiles = glob.glob("./1MostRecentSpeech.txt")
     for f in listOfInformationFiles:
         pdf = PdfPages('1MostRecentSpeeches.pdf')
-        createPlot(parseFile(f),pdf)
+        createPlot(parseFile(f),pdf,f)
         pdf.close()
 def pdf626MostRecentSpeeches():
-    fig = plot.figure(figsize = (8.27, 8.27),dpi = 100)
+    fig = plt.figure(figsize = (8.27, 8.27),dpi = 100)
     listOfInformationFiles = glob.glob("./626MostRecentSpeech.txt")
     for f in listOfInformationFiles:
         pdf = PdfPages('626MostRecentSpeeches.pdf')
-        createPlot(parseFile(f),pdf)
+        createPlot(parseFile(f),pdf,f)
         pdf.close()
 def pdfAllSpeeches():
-    fig = plot.figure(figsize = (8.27, 8.27),dpi = 100)
+    fig = plt.figure(figsize = (8.27, 8.27),dpi = 100)
     listOfInformationFiles = glob.glob("./*RecentSpeech.txt")
     for f in listOfInformationFiles:
         pdf = PdfPages('allSpeeches.pdf')
-        createPlot(parseFile(f),pdf)
+        createPlot(parseFile(f),pdf,f)
         pdf.close()
 if(__name__ == "__main__"):
     if(len(sys.argv) == 2):
@@ -84,10 +103,10 @@ if(__name__ == "__main__"):
         print "worst ratio speeches:"
         for x in range(len(ratioBottomToTop)-1,len(ratioBottomToTop)-11,-1):
             print " "+str(ratioBottomToTop[x])
-       # pdf1MostRecentSpeeches()
-        #pdfMostRecentSpeeches()
-       # pdf626MostRecentSpeeches()
-      #  pdfLeastRecentSpeeches()
-       # pdfAllSpeeches()
+        pdf1MostRecentSpeeches()
+        pdfMostRecentSpeeches()
+        pdf626MostRecentSpeeches()
+        pdfLeastRecentSpeeches()
+        pdfAllSpeeches()
     else:
         print "error, given wrong argument"
